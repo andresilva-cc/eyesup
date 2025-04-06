2cbf825dccd1d24080da8e77cb4da6cf3f983cb6
André Silva <andreluiz.97@outlook.com>
2025-04-06 12:18:49 -0300

    chore: setup tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,19 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineNuxtConfig({
   modules: ['@nuxt/eslint'],
 
   devtools: { enabled: true },
 
+  css: ['~/assets/css/tailwind.css'],
+
   compatibilityDate: '2024-11-01',
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 
   eslint: {
     config: {
