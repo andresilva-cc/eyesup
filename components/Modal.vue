e481d7574659f6402f2f6ef83062c71ae69f0ece
André Silva <andreluiz.97@outlook.com>
2025-04-07 19:47:33 -0300

    feat: add modal component

---
+++ components/Modal.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const showModal = defineModel<boolean>();
+</script>
+
+<template>
+  <div
+    v-if="showModal"
+    class="w-screen h-screen fixed left-0 top-0 flex items-center justify-center"
+  >
+    <div class="w-screen h-screen bg-gray-950/40 backdrop-blur-xs" />
+
+    <div class="min-w-xs absolute p-4 bg-white rounded-xl">
+      <header
+        v-if="$slots.header"
+        class="text-center mb-4"
+      >
+        <slot name="header" />
+      </header>
+
+      <div>
+        <slot name="body" />
+      </div>
+
+      <footer
+        v-if="$slots.footer"
+        class="mt-4"
+      >
+        <slot name="footer" />
+      </footer>
+    </div>
+  </div>
+</template>
